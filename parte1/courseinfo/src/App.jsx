a237140b791688b882fec7189cdb88c26c467808
codecrafteddesings <167926440+codecrafteddesings@users.noreply.github.com>
2025-03-31 16:46:09 -0400

    refactor: mejorar el componente Content y Total para manejar múltiples partes y ejercicios

--- parte1/courseinfo/src/App.jsx
+++ parte1/courseinfo/src/App.jsx
@@ -2,24 +2,60 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>;
 };
 
-const Content = ({ parte1 }) => {
-  return <p>{parte1}</p>;
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
 };
 
-const Total = ({ exercises1 }) => {
-  return <p>{exercises1}</p>;
+const Content = ({
+  parte1,
+  exercises1,
+  parte2,
+  exercises2,
+  part3,
+  exercises3,
+}) => {
+  return (
+    <div>
+      <Part name={parte1} exercises={exercises1} />
+      <Part name={parte2} exercises={exercises2} />
+      <Part name={part3} exercises={exercises3} />
+    </div>
+  );
+};
+
+const Total = ({ exercises1, exercises2, exercises3 }) => {
+  return <p>Number of exercises{exercises1 + exercises2 + exercises3}</p>;
 };
 
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
   const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
     <>
       <Header course={course} />
-      <Content parte1={part1} />
-      <Total exercises1={exercises1} />
+      <Content
+        parte1={part1}
+        exercises1={exercises1}
+        parte2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </>
   );
 };
