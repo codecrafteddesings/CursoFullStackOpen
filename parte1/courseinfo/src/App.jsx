d56ac8c8108f1e38d8642fe1a22b3437fa1c30cb
codecrafteddesings <codecrafteddesings@gmail.com>
2025-04-01 01:03:21 -0400

    chore: add initial files and configurations for Vite and various dependencies

--- parte1/courseinfo/src/App.jsx
+++ parte1/courseinfo/src/App.jsx
@@ -1,4 +1,4 @@
-const Header = ({ course }) => {
+/* const Header = ({ course }) => {
   return <h1>{course}</h1>;
 };
 
@@ -29,33 +29,41 @@ const Content = ({
 
 const Total = ({ exercises1, exercises2, exercises3 }) => {
   return <p>Number of exercises{exercises1 + exercises2 + exercises3}</p>;
-};
+}; */
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const parts = [
+    {
+    name: "Fundamentals of React",
+    exercises: 10,
+  },
+   {
+    name: "Using props to pass data",
+    exercises: 7,
+  },
+   {
+    name: "State of a component",
+    exercises: 14,
+  },
+]
+
+  
 
   return (
     <>
-      <Header course={course} />
-      <Content
-        parte1={part1}
-        exercises1={exercises1}
-        parte2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      
+      <h1>{course}</h1>
+      <p>
+        {part1.name} {part1.exercises}
+      </p>
+      <p>
+        {part2.name} {part2.exercises}
+      </p>
+      <p>
+        {part3.name} {part3.exercises}
+      </p>
+     
     </>
   );
 };
