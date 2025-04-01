ab8f6035ca02a87ef590afd92b56584b2c25c4a9
codecrafteddesings <codecrafteddesings@gmail.com>
2025-04-01 01:03:54 -0400

    refactor: simplify App component structure and utilize object for course data

--- parte1/courseinfo/src/App.jsx
+++ parte1/courseinfo/src/App.jsx
@@ -1,69 +1,35 @@
-/* const Header = ({ course }) => {
-  return <h1>{course}</h1>;
-};
-
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  );
-};
-
-const Content = ({
-  parte1,
-  exercises1,
-  parte2,
-  exercises2,
-  part3,
-  exercises3,
-}) => {
-  return (
-    <div>
-      <Part name={parte1} exercises={exercises1} />
-      <Part name={parte2} exercises={exercises2} />
-      <Part name={part3} exercises={exercises3} />
-    </div>
-  );
-};
-
-const Total = ({ exercises1, exercises2, exercises3 }) => {
-  return <p>Number of exercises{exercises1 + exercises2 + exercises3}</p>;
-}; */
-
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-    name: "Fundamentals of React",
-    exercises: 10,
-  },
-   {
-    name: "Using props to pass data",
-    exercises: 7,
-  },
-   {
-    name: "State of a component",
-    exercises: 14,
-  },
-]
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
-  
+   // Desestructuración
+   const { name, parts } = course;
 
   return (
     <>
-      
-      <h1>{course}</h1>
-      <p>
-        {part1.name} {part1.exercises}
-      </p>
-      <p>
-        {part2.name} {part2.exercises}
-      </p>
-      <p>
-        {part3.name} {part3.exercises}
-      </p>
-     
+      <h1>{name}</h1>
+      <div>
+        {parts.map((part) => (
+          <p key={part.name}>
+            {part.name} {part.exercises}
+          </p>
+        ))}
+      </div>
     </>
   );
 };
