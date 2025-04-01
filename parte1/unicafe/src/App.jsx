7f0e3b80d16123d6031f3b872867ac56351aeda6
codecrafteddesings <167926440+codecrafteddesings@users.noreply.github.com>
2025-04-01 14:28:10 -0400

    refactor: actualizar el título de la aplicación y eliminar archivos CSS innecesarios

--- parte1/unicafe/src/App.jsx
+++ parte1/unicafe/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Hello = (props) => {
+  const bornYear = () => {
+    const yeatNow = new Date().getFullYear()
+    return yeatNow - props.age;
+  }
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+      <p>
+        Hello you are probably born in  {bornYear()} years old
+      </p>
+    </div>
+  );
+};
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const name = "Peter";
+  const age = 10;
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Greetings</h1>
+      <Hello name="Maria" age={20 + 5} />
+      <Hello name={name} age={age} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
