7f40755cc371d666e16358d6196e2247980a66a4
codecrafteddesings <167926440+codecrafteddesings@users.noreply.github.com>
2025-04-02 10:36:10 -0400

    refactor: agregar lógica para calcular y mostrar estadísticas de retroalimentación

--- parte1/unicafe/src/App.jsx
+++ parte1/unicafe/src/App.jsx
@@ -5,7 +5,16 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  console.log(good, neutral);
+  console.log(good, neutral, bad);
+  // guarda el total de clics en una sola variable
+  const total = good + neutral + bad;
+  // guarda el porcentaje de buenos clics en una sola variable
+  const positive = (good / total) * 100;
+  // guarda el porcentaje de malos clics en una sola variable
+  const negative = (bad / total) * 100;
+  // guarda el porcentaje de neutros clics en una sola variable
+  const neutralPercentage = (neutral / total) * 100;
+
   return (
     <>
       <div>
@@ -17,6 +26,14 @@ const App = () => {
         {neutral}
         <button onClick={() => setBad(bad + 1)}>Bad</button>
         {bad}
+        <h1>Statistics</h1>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total: {total}</p>
+        <p>Positive: {positive} %</p>
+        <p>Negative: {negative} %</p>
+        <p>Neutral: {neutralPercentage} %</p>
       </div>
     </>
   );
