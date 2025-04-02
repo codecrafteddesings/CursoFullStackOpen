bfa7c861742cc05b7ca5bf72c96fd47825651307
codecrafteddesings <167926440+codecrafteddesings@users.noreply.github.com>
2025-04-02 10:20:44 -0400

    refactor: mejorar la estructura del componente App y agregar lógica de retroalimentación

--- parte1/unicafe/src/App.jsx
+++ parte1/unicafe/src/App.jsx
@@ -1,17 +1,25 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
-  //const [good, setGood] = useState(0)
-  //const [neutral, setNeutral] = useState(0)
-  //const [bad, setBad] = useState(0)
-
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  console.log(good, neutral);
   return (
-    <div>
-      code here
-    </div>
-  )
-}
+    <>
+      <div>
+        <h1>Give feedback</h1>
+        {good}
+        <button onClick={() => setGood(good + 1)}>Good</button>
 
+        <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+        {neutral}
+        <button onClick={() => setBad(bad + 1)}>Bad</button>
+        {bad}
+      </div>
+    </>
+  );
+};
 
 export default App;
