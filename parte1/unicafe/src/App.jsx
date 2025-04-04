17360eea7db441ee81e26fab6a616a0122fe0df8
codecrafteddesings <167926440+codecrafteddesings@users.noreply.github.com>
2025-04-04 15:37:22 -0400

    refactor: extraer componente de estadísticas y mejorar la presentación de datos en el componente App

--- parte1/unicafe/src/App.jsx
+++ parte1/unicafe/src/App.jsx
@@ -1,5 +1,29 @@
 import { useState } from "react";
 
+const Statitistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = total > 0 ? (good - bad) / total : 0;
+  const positivePercentage = total > 0 ? (good / total) * 100 : 0;
+
+  return (
+    <div>
+      <h3>Statistics</h3>
+      {total > 0 ? (
+        <>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>All: {total}</p>
+          <p>Average: {average.toFixed(1)}</p>
+          <p>Positive: {positivePercentage.toFixed(1)} %</p>
+        </>
+      ) : (
+        <p>No feedback given</p>
+      )}
+    </div>
+  );
+};
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0);
@@ -18,7 +42,6 @@ const App = () => {
     setBad(bad + 1);
     console.log("No me gusta");
   };
-  // calcula el total de clics
 
   return (
     <>
@@ -27,13 +50,7 @@ const App = () => {
         <button onClick={handleGoodClick}>Good</button>{" "}
         <button onClick={handleNeutralClick}>Neutral</button>{" "}
         <button onClick={handleBadClick}>Bad</button>
-        <h3>Statistics</h3>
-        <p>Good: {good}</p>
-        <p>Neutra: {neutral}</p>
-        <p>Bad: {bad}</p>
-        <p>All: {good + neutral + bad}</p>
-        <p>Average: {(good - bad) / (good + neutral + bad)}</p>
-        <p>Positive: {(good / (good + neutral + bad)) * 100} %</p>
+        <Statitistics good={good} neutral={neutral} bad={bad}/>
       </div>
     </>
   );
