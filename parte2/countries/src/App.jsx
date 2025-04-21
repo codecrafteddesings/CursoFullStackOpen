2875b08cb93833da591ad4d2bee6ec85ebc72671
codecrafteddesings <167926440+codecrafteddesings@users.noreply.github.com>
2025-04-21 16:57:22 -0400

    refactor: mejorar la legibilidad del componente App, estandarizar comillas y simplificar el manejo de notas

--- parte2/countries/src/App.jsx
+++ parte2/countries/src/App.jsx
@@ -1,41 +1,56 @@
-import { useState } from 'react'
-import Note from './components/Note'
+import { useState } from "react";
+import Note from "./components/Note";
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('a new note...')
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
 
   const addNote = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const noteObject = {
       content: newNote,
       important: Math.random() > 0.5,
       id: String(notes.length + 1),
-    }
+    };
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-  }
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const handleShowall = () => {
+    setShowAll(!showAll);
+  };
 
   return (
     <div>
       <h1>Notes</h1>
+      <button onClick={handleShowall}>
+        {showAll ? "Show only important" : "Show all"}
+      </button>
       <ul>
-        {notes.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
+        {notes
+          .filter((note) => {
+            if (showAll === true) {
+              return note;
+              return note.important === true;
+            }
+          })
+          .map((note) => (
+            <Note key={note.id} note={note} />
+          ))}
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
