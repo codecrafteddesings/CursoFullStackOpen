7beabd943b0d2af35344470146d496e043dd6e12
codecrafteddesings <codecrafteddesings@gmail.com>
2025-04-29 23:18:36 -0400

    refactor: improve component structure by consolidating Part and Total components in App

--- parte3/src/App.jsx
+++ parte3/src/App.jsx
@@ -2,26 +2,49 @@ const App = () => {
   const course = "Half Stack aplication developen! ";
 
   const parte1 = "Fundamentals of React";
-
   const exercises1 = 10;
+
+  const parte2 = "Using props to pass data";
+  const exercises2 = 7;
+
+  const parte3 = "State of a component";
+  const exercises3 = 14;
 
   const Header = ({ course }) => {
     return <h1>{course}</h1>;
   };
 
-  const Content = ({ parte1 }) => {
-    return <p>{parte1}</p>;
+  const Part = ({ name, exercises }) => {
+    return (
+      <p>
+        {name} {exercises}
+      </p>
+    );
   };
 
-  const Total = ({ exercises1 }) => {
-    return <p>{exercises1}</p>;
+  const Content = () => {
+    return (
+      <div>
+        <Part name={parte1} exercises={exercises1} />
+        <Part name={parte2} exercises={exercises2} />
+        <Part name={parte3} exercises={exercises3} />
+      </div>
+    );
+  };
+
+  const Total = () => {
+    return (
+      <p>
+        Total exersices: {exercises1} + {exercises2} + {exercises3}
+      </p>
+    );
   };
 
   return (
     <div>
       <Header course={course} />
-      <Content parte1={parte1} />
-      <Total exercises1={exercises1} />
+      <Content />
+      <Total />
     </div>
   );
 };
