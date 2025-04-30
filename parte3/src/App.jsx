9922c88c3636eaed769908e5d66027de342f2b28
codecrafteddesings <codecrafteddesings@gmail.com>
2025-04-29 21:52:04 -0400

    fix: correct spelling in course title and improve component structure in App

--- parte3/src/App.jsx
+++ parte3/src/App.jsx
@@ -1,26 +1,28 @@
 const App = () => {
-  const course = "Half Stack application development";
-  
+  const course = "Half Stack aplication developen! ";
 
+  const parte1 = "Fundamentals of React";
 
-  const Header = ({course}) => {
-    return (
-      <h1>{course}</h1>
-    )
-    
-  }
+  const exercises1 = 10;
 
-  
-    
+  const Header = ({ course }) => {
+    return <h1>{course}</h1>;
+  };
 
-  
+  const Content = ({ parte1 }) => {
+    return <p>{parte1}</p>;
+  };
+
+  const Total = ({ exercises1 }) => {
+    return <p>{exercises1}</p>;
+  };
+
   return (
-    
-      <div>
-        <Header course={course} />
-      
-      </div>
-      
+    <div>
+      <Header course={course} />
+      <Content parte1={parte1} />
+      <Total exercises1={exercises1} />
+    </div>
   );
 };
 
