e3d422c893c538acd36917f7371221937b88007d
codecrafteddesings <167926440+codecrafteddesings@users.noreply.github.com>
2025-05-06 10:07:01 -0400

    refactor: clean up Lista component code; improve readability and maintainability

--- parte3/src/components/Lista.jsx
+++ parte3/src/components/Lista.jsx
@@ -1,40 +1,34 @@
 import { useState, Fragment } from "react";
 
 export const Lista = () => {
-
   const [array, setArray] = useState([1, 2, 3, 4, 5]);
   const agregar = () => {
     setArray([
       ...array,
       //6 + 1
-      array.length + 1
-    ])
-  }
+      array.length + 1,
+    ]);
+  };
 
   const eliminar = () => {
     setArray(
       array.filter((item, index) => {
-        return index !== array.length - 1
-      }),
-    )
-  }
-
+        return index !== array.length - 1;
+      })
+    );
+  };
 
   return (
-  <Fragment>
-    <h1>Lista</h1>
-    <ul>
-      {array.map((item, index) => (
-        <li key={index}> - {item}</li>
-      ))}
-    </ul>
-    <button onClick={agregar}>
-      Agregar
-    </button>
-
-    <button onClick={eliminar}>
-      Eliminar
-    </button>
-  </Fragment>
-    )
+    <Fragment>
+      <h1>Lista</h1>
+      <ul>
+        {array.map((item, index) => (
+          <li key={index}> - {item}</li>
+        ))}
+      </ul>
+      <button onClick={agregar}>Agregar</button>
+      {"  "}
+      <button onClick={eliminar}>Eliminar</button>
+    </Fragment>
+  );
 };
